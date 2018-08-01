5ca0e1cfcec1f923daa95252d192a5f048acff3d
vanBroekhoven <timvanbroekhoven@outlook.com>
2018-08-01 17:10:01 +0200

    Large file commited

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -11,9 +11,22 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-sass', get_stylesheet_directory_uri() . '/build/css/child-styles.css', array(), '1.0', 'all' );
 }
 
+function custom_remove_footer_credit () {
+    remove_action( 'storefront_footer', 'storefront_credit', 20 );
+    add_action( 'storefront_footer', 'custom_storefront_credit', 20 );
+}
+
+function custom_storefront_credit() {
+	?>
+	<div class="site-info">
+		&copy; <?php echo get_bloginfo( 'name' ) . ' ' . get_the_date( 'Y' ); ?>
+	</div><!-- .site-info -->
+	<?php
+}
+
 // HOOKS:
 add_action( 'wp_enqueue_scripts', 'storefront_child_scripts' );
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', PHP_INT_MAX);
 
-// Random Shit voor GIT
+add_action( 'init', 'custom_remove_footer_credit', 10 );
