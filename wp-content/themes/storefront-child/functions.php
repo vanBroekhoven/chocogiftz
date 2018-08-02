0544414e044b8b63bc2e77c311d1783eeee7b4d6
vanBroekhoven <timvanbroekhoven@outlook.com>
2018-08-02 15:16:37 +0200

    added function to remove account link in one of the menus for handheld devices AND added comments above every function

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,21 +1,26 @@
 <?php
 
 // FUNCTIONS:
+
+// Enqueue child-theme javascript
 function storefront_child_scripts() {
   wp_enqueue_script( 'extra js', get_stylesheet_directory_uri() . '/js/extra.js');
 }
 
+// Enqueue child-theme stylesheet
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-sass', get_stylesheet_directory_uri() . '/build/css/child-styles.css', array(), '1.0', 'all' );
 }
 
+// Remove credit in footer
 function custom_remove_footer_credit () {
     remove_action( 'storefront_footer', 'storefront_credit', 20 );
     add_action( 'storefront_footer', 'custom_storefront_credit', 20 );
 }
 
+// Put Copyright 'current-year' in footer
 function custom_storefront_credit() {
 	?>
 	<div class="site-info">
@@ -24,9 +29,17 @@ function custom_storefront_credit() {
 	<?php
 }
 
+// Remove the link "my-account" in handheld device 'Big Icon Menu'
+function jk_remove_handheld_footer_links( $links ) {
+	unset( $links['my-account'] );
+	return $links;
+}
+
 // HOOKS:
 add_action( 'wp_enqueue_scripts', 'storefront_child_scripts' );
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', PHP_INT_MAX);
 
 add_action( 'init', 'custom_remove_footer_credit', 10 );
+
+add_filter( 'storefront_handheld_footer_bar_links', 'jk_remove_handheld_footer_links' );
