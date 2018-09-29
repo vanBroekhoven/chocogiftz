7229000260ce774d018dd24ddb859c126eeafcb9
vanbroekhoven <0893122@hr.nl>
2018-09-29 10:58:14 +0200

    now really all pages full-width

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -128,3 +128,18 @@ function remove_storefront_sidebar() {
 	}
 }
 
+add_action( 'wp', 'woa_remove_sidebar_shop_page' );
+function woa_remove_sidebar_shop_page() {
+
+if ( is_shop() || is_tax( 'product_cat' ) || get_post_type() == 'product' ) {
+
+remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
+add_filter( 'body_class', 'woa_remove_sidebar_class_body', 10 );
+}
+}
+
+function woa_remove_sidebar_class_body( $wp_classes ) {
+
+$wp_classes[] = 'page-template-template-fullwidth-php';
+return $wp_classes;
+}
